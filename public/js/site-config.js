e7c9da46058417e8dbb53be5076678719706ea57
Nickolas Gallegos <gallegos.nick99@outlook.com>
2026-03-21 23:40:14 -0500

    fix: update templateCatalogUrl in site-config.js to correct URL

--- public/js/site-config.js
+++ public/js/site-config.js
@@ -1,4 +1,4 @@
 window.SITE_CONFIG = {
   templateCatalogUrl:
-    "https://bcpb-photo-bucket.s3.us-east-2.amazonaws.com/template-catalog/published-templates.json",
+    "https://d3r11bcq65pqz1.cloudfront.net/template-catalog/published-templates.json",
 };
